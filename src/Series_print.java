9603cf3e84f6674d72968779d9e0898b2c989def
faizu186 <sadiazohorafaiza@gmail.com>
2026-07-16 16:00:15 +0600

    even series printing but with sum

--- src/Series_print.java
+++ src/Series_print.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class Series_print {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        int s=0;
         for(int i=2;i<=20;i+=2)
         {
             System.out.print(i+"  ");
+            s=s+i;
         }
         System.out.println();
         int x=2;
@@ -19,6 +21,9 @@ public class Series_print {
             System.out.print(y+"  ");
             y+=2;
         }while(y<=20);
+        System.out.println();
+        System.out.println("sum= "+s);
+
         sc.close();
     }
 }
