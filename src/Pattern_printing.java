7b8937e83b98b68e83fe3a65f1aed93abca2a4aa
faizu186 <sadiazohorafaiza@gmail.com>
2026-08-11 12:12:17 +0600

    pattern printing

---
+++ src/Pattern_printing.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Pattern_printing {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of pattern: ");
+        int x=sc.nextInt();
+        System.out.println("here is your pattern!");
+        for(int i=1;i<=x;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+
+    }
+}
+
+
